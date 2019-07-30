baaf4c4977eccaa41785a25fc3e939e8f61eb697
wanglf1207 <wanglf1207@163.com>
2019-07-30 16:21:17 +0800

    init commit

---
+++ Servlet-HelloWorld/src/com/demo/servlet/HelloWorldServlet.java
@@ -0,0 +1,28 @@
+package com.demo.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloWorldServlet extends HttpServlet {
+
+    public String message;
+
+    public void init() throws ServletException {
+        message = "Hello Servlet";
+    }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.write("<h1>" + message + "</h1>");
+    }
+}
